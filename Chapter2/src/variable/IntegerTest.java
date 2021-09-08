8065125d64e0645fb106f6ff47e67161359cbc5b
Leehojun12 <dlghwns82@naver.com>
2021-09-08 12:46:13 +0900

    docs: Second_Java

---
+++ Chapter2/src/variable/IntegerTest.java
@@ -0,0 +1,13 @@
+package variable;
+
+public class IntegerTest {
+	
+	public static void main(String[] args) {
+		byte bs = -128; // -128~127
+		System.out.println(bs);
+		
+		//int iVal = 12345678900;
+		long IVal = 12345678900L;
+	}
+
+}
---
+++ Chapter2/src/variable/VarialbeTest.java
@@ -0,0 +1,13 @@
+package variable;
+
+public class VarialbeTest {
+	
+	public static void main(String[] args) {
+		int age, count; //¼±¾ð 
+		age = 10;	
+		System.out.println(age);
+		
+		int numberOfStudent; // camel notation 
+	}
+
+}
