b602ea6ad2bd4241bcc81fd8bd0177c7e80a87a5
Leehojun12 <dlghwns82@naver.com>
2021-10-02 22:05:02 +0900

    docs:Update

--- Chapter9/src/interfaceex/Calc.java
+++ Chapter9/src/interfaceex/Calc.java
@@ -10,4 +10,25 @@ public interface Calc {
 	int times(int num1, int num2);
 	int divide(int num1, int num2);
 	
+	default void description() {
+		System.out.println("정수 계산기 구현");
+		//myMethod();
+	}
+	static int total(int[] arr) {
+		int total = 0;
+		
+		for(int i: arr) {
+			total += i;
+		}
+		//mystaticMethod();
+		return total;
+	}
+	
+	/*private void myMethod() { // java jre 9 가능
+		System.out.println("private method");
+	}*/
+	
+	/*private static void mystaticMethod() { // java jre 9 가능
+		System.out.println("private static method");
+	}*/
 }
