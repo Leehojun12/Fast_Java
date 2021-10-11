dc9ce1f4ebee493b78cf081ff63c78c868c4108a
Leehojun12 <dlghwns82@naver.com>
2021-10-11 18:58:21 +0900

    docs:Reduce

--- Chapter12/src/lambda/TestLambda.java
+++ Chapter12/src/lambda/TestLambda.java
@@ -8,9 +8,11 @@ public class TestLambda {
 	public static void main(String[] args) {
 		PrintString lambdaStr = s-> System.out.println(s);
 		lambdaStr.showString("Test");
-		showMyString(lambdaStr);
+		
+		showMyString(lambdaStr); // showMyString으로 lambda를 반환
+		
 		PrintString test = returnString();// test로 반환
-		test.showString("Test3");
+		test.showString("Test3"); // 반환 되며 returnString 으로 출력
 
 	}
 	public static void showMyString(PrintString p) {
