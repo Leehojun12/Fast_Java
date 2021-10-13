0ff42724bad1bb9f29485ea1fa5d18e0fc44d981
Leehojun12 <dlghwns82@naver.com>
2021-10-13 18:40:18 +0900

    docs:Update

--- Chapter14/src/stream/outputstream/FileOutputTest1.java
+++ Chapter14/src/stream/outputstream/FileOutputTest1.java
@@ -7,7 +7,7 @@ public class FileOutputTest1 {
 
 	public static void main(String[] args) {
 		
-		try(FileOutputStream fos = new FileOutputStream("output.txt", true)){
+		try(FileOutputStream fos = new FileOutputStream("output.txt",true)){
 			fos.write(65);
 			fos.write(66);
 			fos.write(67);
