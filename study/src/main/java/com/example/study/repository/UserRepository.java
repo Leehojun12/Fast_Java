d8ee3d503feffad7b4b8be5da919438bf6d182d9
Leehojun12 <dlghwns82@naver.com>
2021-10-22 18:16:52 +0900

    docs: Update

--- study/src/main/java/com/example/study/repository/UserRepository.java
+++ study/src/main/java/com/example/study/repository/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository //Repository다 선언
 public interface UserRepository extends JpaRepository<User, Long>{
-
+// JPA를 상속받게 되면 기본적인 CRUD를 제공받을 수 있다
 }
--- study/src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ study/src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -2,19 +2,19 @@ package com.example.study.repository;
 
 import com.example.study.StudyApplicationTests;
 import com.example.study.model.entitiy.User;
-
+import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import java.time.LocalDateTime;
-
+import java.util.Optional;
 
 public class UserRepositoryTest extends StudyApplicationTests {
 
     // Dependency Injection (DI)
     @Autowired
-    private UserRepository userRepository; // = new UserRepository(); 생략
+    UserRepository userRepository; // = new UserRepository(); 생략
 
-    @Test //테스트코드를 작성할때 필수 어노테이션
+    @Test//테스트코드를 작성할때 필수 어노테이션
     public void create() {
         // String sql = insert into user(%s, %s, %d) value (account, email, age); 이런식으로 작성- DI (X)
         User user = new User();
@@ -24,22 +24,42 @@ public class UserRepositoryTest extends StudyApplicationTests {
         user.setEmail("dlghwns82@naver.com");
         user.setPhoneNumber("010-8974-2058");
         user.setCreatedAt(LocalDateTime.now());
-        user.setUpdatedBy("admin");
+        user.setCreatedBy("admin");
 
-
-        userRepository.save(user)
+        User newUser = userRepository.save(user);
+        System.out.println("newUser : " + newUser);
 
     }
-
+    @Test
     public void read() {
+        Optional<User> user = userRepository.findById(2L); //Type이 Long이기떄문에 2L;;
 
+        user.ifPresent(selectuser ->{
+            System.out.println("user : " + user);
+        });
     }
-
+    @Test
     public void update() {
+        Optional<User> user = userRepository.findById(2L);
 
+        user.ifPresent(selectuser ->{
+            selectuser.setAccount("pppp");
+            selectuser.setCreatedAt(LocalDateTime.now());
+            selectuser.setCreatedBy("update method()");
+
+            userRepository.save(selectuser);
+        });
     }
-
+    @Test
     public void delete() {
+        Optional<User> user = userRepository.findById(3L);
 
+        Assert.assertTrue(user.isPresent()); //true
+        user.ifPresent(selectUser->{
+            userRepository.delete(selectUser);
+        });
+        Optional<User> deletUser = userRepository.findById(3L);
+
+        Assert.assertFalse(deletUser.isPresent()); //false
     }
 }
